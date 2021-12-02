116ed8017b9c1694f571a6e198a4d82e3bf732d3
EduardoLovo <eduardo.lovo@gmail.com>
2021-12-02 14:44:15 -0300

    create Components, Item, App.styles

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,42 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css';
+import * as C from './Styles/App.styles'
+import {Item} from './Types/Item'
 
 function App() {
+
+  const [list, setList] = useState<Item[]>([
+    {
+      id: 1,
+      name: 'Comprar pão na padaria',
+      done: false
+    },
+    {
+      id: 2,
+      name: 'Comprar um bolo',
+      done: false
+    }
+
+  ]);
+
+
   return (
-    <div className="App">
-      Olá Mundo
-    </div>
+    
+    <C.Container>
+      <C.Area>
+        <C.Header>Lista de Tarefas</C.Header>
+
+        {/* Area de adicionar nova tarefa */}
+        <h2>Adicionar nova tarefa</h2>
+
+        {/* Lista de tarefas */}
+        {list.map((item, index)=>(
+          <div>{item.name}</div>
+        ))}
+
+      </C.Area>
+    </C.Container>
+  
   );
 }
 
