77b9ca130c7d469298f261bb174aa8d7d7cff761
EduardoLovo <eduardo.lovo@gmail.com>
2021-12-02 15:51:50 -0300

    Projeto Finalizado

--- src/App.tsx
+++ src/App.tsx
@@ -31,6 +31,16 @@ function App() {
     setList(newList);
   }
 
+  const handleTaskChange = (id: number, done: boolean) => {
+    let newList = [...list];
+    for(let i in newList) {
+      if(newList[i].id === id) {
+        newList[i].done = done;
+      }
+    }
+    setList(newList);
+  }
+
 
   return (
     
@@ -43,7 +53,11 @@ function App() {
 
         {/* Lista de tarefas */}
         {list.map((item, index)=>(
-          <ListItem key={index} item={item} />
+          <ListItem 
+            key={index} 
+            item={item} 
+            onChange={handleTaskChange}
+          />
         ))}
 
       </C.Area>
--- src/Components/ListItem/index.tsx
+++ src/Components/ListItem/index.tsx
@@ -1,21 +1,21 @@
 import * as C from './styles';
 import { Item } from '../../Types/Item';
-import { useState } from 'react';
 
 type Props = {
-    item: Item
+    item: Item,
+    onChange: (id: number, done: boolean) => void
 }
 
-export const ListItem = ({ item }: Props) => {
+export const ListItem = ({ item, onChange }: Props) => {
 
-    const [isChecked, setIsChecked] = useState(item.done);
+    // const [isChecked, setIsChecked] = useState(item.done);
 
     return (
-        <C.Container done={isChecked}>
+        <C.Container done={item.done}>
             <input 
                 type="checkbox" 
-                checked={isChecked}
-                onChange={e => setIsChecked(e.target.checked)}
+                checked={item.done}
+                onChange={e => onChange(item.id, e.target.checked)}
             />
             <label>{item.name}</label>
         </C.Container>
